cbde214a5ecd340f6dcdb9f2b199e92843052d63
mahi5256 <m.p6092003@gmail.com>
2023-10-20 00:50:12 +0530

    adding tailwind.css

--- social-media-grip/src/App.jsx
+++ social-media-grip/src/App.jsx
@@ -3,7 +3,8 @@ import LoginButton from './components/login';
 import LogoutButton from './components/logout';
 import Profile from './components/profile'; // Make sure to import the Profile component
 import { useAuth0 } from '@auth0/auth0-react';
-import './index.css'
+import './App.css';  // Import your global styles if needed
+
 const App = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
@@ -12,10 +13,11 @@ const App = () => {
   }
 
   return (
-    <div className=''>
+    <div className='container' >
       <header className='header'>
         <nav>
-          <h1 className='orange_gradient'>Login</h1>
+          <h1 className='header_heading glassmorphsim'>social media integration</h1>
+          <p className='header_desc'>Connect,Share,Thrive: Your Social World, All in One App!</p>
         </nav>
       </header>
       <main>
@@ -30,7 +32,6 @@ const App = () => {
           )}
         </section>
       </main>
-
     </div>
   );
 };
