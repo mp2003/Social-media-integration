c4d4a38e625111d5844df0e8e3e996cf86f287a9
mahi5256 <m.p6092003@gmail.com>
2023-10-19 22:26:12 +0530

    adding the login

--- social-media-grip/src/main.jsx
+++ social-media-grip/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Auth0Provider
+    domain="mp2003.au.auth0.com"
+    clientId="MHJlnvdw8DE7HAgFoxdNkgrzZ6jSNZwe"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>,
-)
+  </Auth0Provider>,
+);
