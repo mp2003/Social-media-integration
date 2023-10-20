b6166b6b830ba9ceaf0a9b63d14d9570a559a2f8
mahi5256 <m.p6092003@gmail.com>
2023-10-21 01:28:17 +0530

    styling logout

--- social-media-grip/src/components/profile.jsx
+++ social-media-grip/src/components/profile.jsx
@@ -3,11 +3,12 @@ import './profile.css'
 const Profile = ({ name, email, picture }) => {
 
   return (
-    <div>
-      <img src={picture} className='profile_img' alt='profile' />
-      <h1>{name} </h1>
-      <h3>{email}</h3>
-
+    <div className='userInfo'>
+      <img src={picture} className='profile_img slide-in-blurred-left roll-in-blurred-left' alt='profile' />
+      <div className="name">
+        <h1 className='text-focus-in '>Name: {name} </h1>
+        <h3 className='text-focus-in ' >email: <a href={`mailto:${email}`}>{email}</a></h3>
+      </div>
     </div>
   )
 }
